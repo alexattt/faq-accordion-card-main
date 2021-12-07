1d3407eec0cbff336a4e723f3f7e5c46b72b4ca1
Aleksandra Timofejeva <aleksandra1338@inbox.lv>
2021-12-08 01:44:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function () {
+  const accordion = document.getElementsByClassName('container');
+
+  for (i=0; i<accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
+      this.classList.toggle('active')
+    })
+  }
+})
